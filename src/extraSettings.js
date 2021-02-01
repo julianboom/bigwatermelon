aa1840f55aa14cdbf20ab8baef7e47d0cf3abeea
DESKTOP-Q4NR7LP\caizonglin <171900662@qq.com>
2021-02-02 00:24:12 +0800

    欢迎游戏可玩性

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -6,16 +6,16 @@
 let extraScore = 1000;
 
 // 无敌模式：true 改为 false
-let wuDi = true;
+let wuDi = false;
 
 // 第一个水果：修改数字为 0-10
 let firstFruit = 2;
 
 // 指定生成的水果
 const minRandomFruitNum = 0; // 生成随机水果最小值（0-10）
-const maxRandomFruitNum = 11; // 生成随机水果最大值（1-11）
+const maxRandomFruitNum = 5; // 生成随机水果最大值（1-11）
 let setFruits = {
-  startFruits: [1,0,1,3,6,9], // 指定前几次生成的水果，可填入任意数量的数字
+  startFruits: [], // 指定前几次生成的水果，可填入任意数量的数字
   randomFunction: () => {
     return minRandomFruitNum + Math.floor(Math.random() * (maxRandomFruitNum - minRandomFruitNum));
   }
@@ -34,7 +34,7 @@ let adLink = 'http://resume.anshare.cn';
 document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
 
 // 开启选分弹窗：将 false 改为 true
-let selectModal = true;
+let selectModal = false;
 
 // 水果合成反转：false 改为 true（尚未支持）
 let reverseLevelUp = false;
